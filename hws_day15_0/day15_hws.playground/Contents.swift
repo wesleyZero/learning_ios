9598ab878d4fa3d9779511dff40950ac30daeaf1
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-11-21 08:02:07 -0800

    working on dat 15 now

---
+++ hws_day15_0/day15_hws.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+let actor = "Tom Cruise 🚢"
+
+let quote = "He tapped a sign that said \"believe\""
+
+print(quote)
+
+print(quote.hasPrefix("He"))
+
+
+var counter = 5
+
+for _ in 1...5 {
+    print(counter)
+    
+    counter += 1
+}
+
+print(counter.isMultiple(of: 5))
+
+let id = Int.random(in: 1...1000)
+
+print(id)
+
+
+let score = 3.1
+let score2 = 3.10
+
+var weWon = true
+
+weWon.toggle()
+
+
+let name = "tayor swift"
+let age = 34
+let message = "Hi, my name is \(name) and I am \(age) years old."
+print(message)
+
+
+
+
+
