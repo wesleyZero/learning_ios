a385ba6da6671417fac6053b8c8efb62e73e7894
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-11-22 18:37:23 -0800

    working on dat 15 now

--- hws_day15_0/day15_hws.playground/Contents.swift
+++ hws_day15_0/day15_hws.playground/Contents.swift
@@ -37,7 +37,16 @@ let age = 34
 let message = "Hi, my name is \(name) and I am \(age) years old."
 print(message)
 
+var colors = ["red" , "blue", "green"]
+
+print(colors[1])
+
+colors.append("poop")
 
 
+let employee = [ "steve" : 45,
+                 "tim": 22]
 
+let nums1 = Set([1,5,2,78 ,21,311])
 
+nums1.contains(21)
