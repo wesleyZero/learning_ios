addb811daed4d75080a58ad5f3fdf0050773f1c2
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-11-28 11:16:16 -0800

    button working

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -18,22 +18,30 @@ struct ContentView: View {
 //        .padding()
 //    }
     
-    var body: some View {
-        NavigationStack{
-            Form {
-                Section {
-                    Text("Hello career that actually makes me happy")
-                    Text("Engineering makes me happy")
-                }
-                Section {
-                    Text("This new section of text is helping me learn")
-                }
-            }
-        .navigationTitle(" ")
-        .navigationBarTitleDisplayMode(.inline)
-        }
-    }
+//    var body: some View {
+//        NavigationStack{
+//            Form {
+//                Section {
+//                    Text("Hello career that actually makes me happy")
+//                    Text("Engineering makes me happy")
+//                }
+//                Section {
+//                    Text("This new section of text is helping me learn")
+//                }
+//            }
+//        .navigationTitle(" ")
+//        .navigationBarTitleDisplayMode(.inline)
+//        }
+//    }
+  
+    @State private var tapCount: Int = 0
     
+    var body: some View {
+        Button("Tap Count \(tapCount)") {
+            tapCount += 1
+        }
+        
+    }
     
 }
 
