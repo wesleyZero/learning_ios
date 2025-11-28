45c237ae2968b27c4482b7955c25a3f178f783d7
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-11-28 14:54:08 -0800

    button working

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,6 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedStudent = "x"
+    let students = ["Harry", "Ron", "Luna", "Draco"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text ($0)
+                    }
+                }
+            }
+            .navigationTitle("Select a student")
+        }
+    }
+    
+    
 //    var body: some View {
 //        VStack {
 //            Image(systemName: "globe")
@@ -45,13 +63,13 @@ struct ContentView: View {
 //    }
     
     
-    @State private var name = ""
-    var body: some View {
-        Form {
-            TextField("Enter your name: ", text: $name)
-            Text("Hello, \(name)")
-        }
-    }
+//    @State private var name = ""
+//    var body: some View {
+//        Form {
+//            TextField("Enter your name: ", text: $name)
+//            Text("Hello, \(name)")
+//        }
+//    }
     
 }
 
