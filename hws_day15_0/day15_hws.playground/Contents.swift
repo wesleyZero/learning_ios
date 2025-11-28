efa3671ab5881ec9d713737db1279eb9f0620c3b
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-11-27 16:18:51 -0800

    working on dat 15 now

--- hws_day15_0/day15_hws.playground/Contents.swift
+++ hws_day15_0/day15_hws.playground/Contents.swift
@@ -1,69 +1,24 @@
 import UIKit
 
-let greet = { print("Hello, world!")}
 
-let greet2 = { (name: String) in
-    print("Hello \(name)")
+
+
+enum weekday {
+    case monday, tuesday, wednesday, thursday, saturday, sunday
 }
 
-greet()
+var dayOfWeek: weekday = weekday.monday
 
-greet2("John")
-
-let team = ["Gloria", "stavros", "John"]
-
-let onlyT = team.filter( { (name: String) in name.hasPrefix("J") })
+dayOfWeek = .tuesday
 
 
-let movie = """
-This is a long quote
-that can go onto multiple lines
-"""
-    
-print(movie)
+var numbers = Set([2,2,2,2,6,22,5,22,563,122,545])
 
-movie.count
+numbers.contains(5)
 
-movie.hasPrefix("This")
-movie.hasPrefix("nope")
+numbers.contains(563)
 
-var score = 5
-score += 1
-
-let id = score.isMultiple(of: 4)
-let rand = Int.random(in: 1...105)
-
-let scor2 = 4.938939
-
-var dogsAreGood = true
-
-dogsAreGood.toggle()
-
-var colors = [ "Blue", "Red", "Orange" ]
-
-colors.append("SuperBlue")
-
-colors.remove(at: 2)
-
-colors.contains("Blue")
-
-var employee = [
-    "Name" : "John",
-    "Age" : "29"
-]
-
-employee["Name", default: "Def value"]
-
-
-employee["Turd", default: "nope"]
-
-
-
-
-
-
-
-
+numbers.insert(111)
 
 
 
