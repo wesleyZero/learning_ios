2f0c44e06f291b828034c684ff5baf6d94445c5e
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-11-28 08:50:34 -0800

    working on dat 15 now

--- hws_day15_0/day15_hws.playground/Contents.swift
+++ hws_day15_0/day15_hws.playground/Contents.swift
@@ -13,5 +13,29 @@ var game = Game()
 
 game.score = 10
 
-game.score = 100 
+game.score = 100
 
+
+struct User {
+    let id: Int
+    let name: String
+    let age: Int
+}
+
+
+func parseUser(from raw: [[String: Any]]) -> [User] {
+
+    
+    var ret = [] as [User]
+    
+    for data in raw {
+        if let id = data["id"] as? Int,
+           let name = data["name"] as? String,
+           let age = data["age"] as? Int{
+            var usr = User(id: id, name: name, age: age)
+            ret.append(usr)
+        }
+        
+    }
+    return ret
+}
