b7126ca5285f7d424783c3c2fedbb757ac30d0d7
Wesley Johanson <wesleyJohansonZero@gmail.com>
2025-12-09 15:29:21 -0800

    fnished the wesplit app and now adding exceptions

--- hws_prj1_day2/hws_prj1_day2/ContentView.swift
+++ hws_prj1_day2/hws_prj1_day2/ContentView.swift
@@ -10,16 +10,28 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numOfPpl = 2
-    @State private var tipPercentage = 20.0
+    @State private var tipPercentage = 20
+    private let MIN_NUM_PPL: Int = 2
+
+    private var checkWithTip: Double {
+        checkAmount * (1 + Double(tipPercentage) / 100.0)
+    }
+    private var checkWithTipPerPerson: Double {
+        checkWithTip / Double(numOfPpl)
+    }
     
     @FocusState private var amountIsFocused: Bool
     
     var tipPercentages = [10, 15, 20, 25, 30, 0]
     
+    @State private var exceptions: [String] = []
+    
+    
     
     var body: some View {
         NavigationStack {
             Form {
+                //FORM 1
                 Section {
                     TextField(
                         "Amount",
@@ -32,25 +44,61 @@ struct ContentView: View {
                     .focused($amountIsFocused)
                     
                     Picker("Number of People", selection: $numOfPpl) {
-                        ForEach(2..<100) {
+                        ForEach(MIN_NUM_PPL..<100) {
                             Text("\($0)")
                         }
                     }
                     
-                    
-                    
-
+//                    Text("How much would you like to tip?")
+//                    Picker("Tip Percentage", selection: $tipPercentage) {
+//                        ForEach(tipPercentages, id: \.self) {
+//                            Text("\($0)%")
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
                 }
-                Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                
+                Section ("How much do you want to tip?") {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text("\($0)%")
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
+                
+            
+                
+                Section("Check with Tip") {
+                    Text(checkWithTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Check with Tip / Person"){
+                    Text(checkWithTipPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Check with Tip For person A"){
+//                    TextField()
+                    Text(checkWithTipPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Button {
+                        let newLabel = "Exception \(exceptions.count + 1)"
+                        exceptions.append(newLabel)
+                        print(exceptions)
+                    } label: {
+                        Label("Add exception", systemImage: "plus.circle.fill")
+                            .foregroundStyle(.blue)
+                    }
+                }
+                
+                
+                
             }
             .navigationTitle("WeSplit")
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
                     Button("Done") {
-                        amountIsFocused.toggle()
+                        amountIsFocused = false
                     }
                     Button("Print Tip %") {
                         print("tip percentage is \(tipPercentage)")
